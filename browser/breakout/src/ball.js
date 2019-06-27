9999ff495d86fe4f95264e8f328c40abc22291a3
Chris Leveille <cdleveille@gmail.com>
2019-06-27 00:50:23 -0400

    starting browser-based javascript/HTML5 port

---
+++ browser/breakout/src/ball.js
@@ -0,0 +1,27 @@
+export default class Ball {
+    constructor(gameWidth, gameHeight) {
+        this.image = document.getElementById('img_ball');
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.position = { x: 10, y: 10 };
+        this.speed = { x: 3, y: 3 };
+        this.size = 16;
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.size, this.size);
+    }
+
+    update(deltaTime) {
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+
+        if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
+            this.speed.x = -this.speed.x;
+        }
+
+        if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
+            this.speed.y = -this.speed.y;
+        }
+    }
+}
---
+++ browser/src/index.js
@@ -0,0 +1,8 @@
+// http-server -a localhost -p 8000 -c-1
+
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext('2d');
+
+const GAME_WIDTH = canvas.width;
+const GAME_HEIGHT = canvas.height;
+
